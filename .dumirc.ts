3bb9405dfaaf1269d5c4a8a27e7b9507adb4b9a2
李哈哈 <lihawhaw@163.com>
2023-04-29 11:22:33 +0800

    更新 .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'dumi';
 import { defineThemeConfig } from 'dumi-theme-chakra';
 
 export default defineConfig({
-  base: '/lyzjs/',
-  publicPath: '/lyzjs/',
+  base: '/', // '/lyzjs/',
+  publicPath: '/', // '/lyzjs/',
   outputPath: 'docs-dist',
   esbuildMinifyIIFE: true,
   // all in one to fix ConfigProvider error
